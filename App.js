f14415f4d7742163c74527ca4a668578613e5452
Hatice-Kubra <85344166+Hatice-Kubra@users.noreply.github.com>
2021-06-22 15:43:10 +0300

    src

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <Sidebar/>
+ 
+     <Feed/>
+
+     {/*Widgets*/}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Feed.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Feed.css";
+
+function Feed () {
+    return (
+        <div>
+            {/*Header*/}
+            <div className="Feed">
+            <h2>Anasayfa</h2>
+            </div>
+
+            {/*Tweet Box*/}
+
+            {/*Post*/}
+            {/*Post*/}
+            {/*Post*/}
+            {/*Post*/}
+            {/*Post*/}
+            {/*Post*/}
+            {/*Post*/}
+
+        </div>
+    );
+}
+
+export default Feed;
